04961af217a12335c7839f715db4632ce52d9684
Patrick Groot <info@patrickgroot.com>
2022-02-12 16:10:48 +0100

    Update NewZealandLicensePlate.php

--- src/NewZealandLicensePlate.php
+++ src/NewZealandLicensePlate.php
@@ -125,8 +125,8 @@ class NewZealandLicensePlate extends AbstractLicensePlate implements LicensePlat
          */
         $this->sideCodes = [
             // Cars and heavy vehicles
-            1  => "/^((?!{$exceptionsTwoLetterRegex})[A-Za-z]{2})([1-9][0-9]{0,4})$/ui",         // 1964 - 1996.
-            2  => "/^((?={$exceptions01})[A-Za-z]{2})([1-9][0-9]{0,4})$/ui", // 1996 - 2001.
+            1  => "/^((?!{$exceptionsTwoLetterRegex})[A-Za-z]{2})([1-9][0-9]{0,4})$/ui", // 1964 - 1996.
+            2  => "/^((?={$exceptions01})[A-Za-z]{2})([1-9][0-9]{0,4})$/ui",             // 1996 - 2001.
             // Needs fix starting point of the plate UR for correctness and exceptions 1 to 3. 
             3  => "/^((?!{$exceptions01})[a-zA-Z]{2})([1-9]\d\d\d)$/ui",                 // 1996 - 2001.
             4  => "/^((?!{$exceptionsThreeLetterRegex})[A-Za-z]{3})([1-9]\d\d)$/ui",     // 2001 - now.
